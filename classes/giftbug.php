0cddf94da7770f0fd84b82031b6e7ada611e3540
icealleymedia <32878819+icealleymedia@users.noreply.github.com>
2018-02-22 15:50:23 -0500

    main class with methods

--- classes/giftbug.php
+++ classes/giftbug.php
@@ -2,5 +2,30 @@
 class GiftBug{
 	
 	
+	function __construct(){
+		
+		$this->dbHost = $dbHost;
+		$this->dbName = $dbName;
+		$this->dbUser = $dbUser;
+		$this->dbPass = $dbPass;
+		
+		$this->dbConn = 'mysql:host=' . $this->dbHost . ';dbname=' . $this->dbName;
+		
+		$this->get_settings();
+	}
+	
+	function get_settings(){
+		
+		$dbh = new PDO($this->dbConn, $this->dbUser, $this->dbPass);
+		
+	}
+	
+	function set_settings(){
+		
+	}
+	
+	function update_settings(){
+		
+	}
 }
 ?>
